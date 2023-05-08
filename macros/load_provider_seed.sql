49df5e23875b10f4c47314de7b6c2f3281b18da9
tuvaforrest <forrest@tuvahealth.com>
2023-05-08 12:22:19 -0400

    interim updates

---
+++ macros/load_provider_seed.sql
@@ -0,0 +1,88 @@
+{% macro load_provider_seed(uri,pattern) %}
+{{ return(adapter.dispatch('load_provider_seed')(uri,pattern)) }}
+{% endmacro %}
+
+
+
+{% macro redshift__load_provider_seed(uri,pattern) %}
+{% set sql %}
+copy  {{ this }}
+  from 's3://{{ uri }}/{{ pattern }}'
+  access_key_id 'AKIA2EPVNTV4FLAEBFGE'
+  secret_access_key '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'
+  csv
+  gzip
+  region 'us-east-1'
+
+{% endset %}
+
+{% call statement('redsql',fetch_result=true) %}
+{{ sql }}
+{% endcall %}
+
+{% if execute %}
+{# debugging { log(sql, True)} #}
+{% set results = load_result('redsql') %}
+{{ log("Loaded data from external s3 resource\n  loaded to: " ~ this ~ "\n  from: s3://" ~ uri ,True) }}
+{# debugging { log(results, True) } #}
+{% endif %}
+
+{% endmacro %}
+
+
+
+{% macro snowflake__load_provider_seed(uri,pattern) %}
+{% set sql %}    
+copy into {{ this }}
+    from s3://{{ uri }}
+    file_format = (type = CSV
+    compression = 'GZIP'
+    field_optionally_enclosed_by = '"'
+)
+pattern = '.*\/{{pattern}}.*';
+{% endset %}
+{% call statement('snowsql',fetch_result=true) %}
+{{ sql }}
+{% endcall %}
+
+{% if execute %}
+{# debugging { log(sql, True)} #}
+{% set results = load_result('snowsql') %}
+{{ log("Loaded data from external s3 resource\n  loaded to: " ~ this ~ "\n  from: s3://" ~ uri ~ "/" ~ pattern ~ "*\n  rows: " ~ results['data']|sum(attribute=2),True) }}
+{# debugging { log(results, True)} #}
+{% endif %}
+
+{% endmacro %}
+
+
+
+
+{% macro bigquery__load_provider_seed(uri,pattern) %}
+{% set sql %}
+load data into {{ this }}
+from files (format = 'csv',
+    uris = ['gs://{{ uri }}/{{ pattern }}*'],
+    compression = 'GZIP',
+    quote = '"'
+    )
+{% endset %}
+
+{% call statement('bigsql',fetch_result=true) %}
+{{ sql }}
+{% endcall %}
+
+{% if execute %}
+{% set results = load_result('bigsql') %}
+{{ log(sql, True)}}
+{{ log("Loaded data from external gs resource\n  loaded to: " ~ this ~ "\n  from: gs://" ~ uri ~ "/" ~ pattern ~ "*",True) }}
+{{ log(results, True)}}
+{% endif %}
+
+{% endmacro %}
+
+{% macro default__load_provider_seed(uri,pattern) %}
+{% if execute %}
+{% do log('No adapter found, seed not loaded',info = True) %}
+{% endif %}
+
+{% endmacro %}
