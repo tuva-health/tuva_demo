52dd9ae6ae2e62cf7baf59c8c51aad6c18395da1
thutuva <101137626+thutuva@users.noreply.github.com>
2024-04-30 15:51:50 -0400

    upgrade to 8.0 of ttp (#37)

--- models/patient.sql
+++ models/patient.sql
@@ -7,6 +7,7 @@ cast(null as {{ dbt.type_string() }} ) as patient_id
 , cast(null as date) as birth_date
 , cast(null as date) as death_date
 , cast(null as {{ dbt.type_int() }} ) as death_flag
+, cast(null as {{ dbt.type_string() }} ) as social_security_number
 , cast(null as {{ dbt.type_string() }} ) as address
 , cast(null as {{ dbt.type_string() }} ) as city
 , cast(null as {{ dbt.type_string() }} ) as state
